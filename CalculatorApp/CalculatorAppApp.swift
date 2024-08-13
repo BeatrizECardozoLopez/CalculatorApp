d3b16dec43f7b774ebff4a603a7e3f9272bf4fb8
BeaCardozo <113352853+BeaCardozo@users.noreply.github.com>
2024-08-12 22:01:55 -0400

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Beatriz Cardozo on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalculatorApp
+//
+//  Created by Beatriz Cardozo on 12/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
